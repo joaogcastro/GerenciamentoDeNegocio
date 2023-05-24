f6cc69604994a4d1beaefb1019aaa015d780f342
Joao Castro <110193153+joaogcastro@users.noreply.github.com>
2023-05-24 11:22:04 -0300

    Atualização Entidades
    Cliente

--- projetosistemabuffet/src/models/Cliente.java
+++ projetosistemabuffet/src/models/Cliente.java
@@ -1,5 +1,56 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Cliente {
-    
+    private int id;
+    private String cpf;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private static ArrayList<Festa> historicoFestas = new ArrayList<Festa>();
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public static ArrayList<Festa> getHistoricoFestas() {
+        return historicoFestas;
+    }
+    public static void setHistoricoFestas(ArrayList<Festa> historicoFestas) {
+        Cliente.historicoFestas = historicoFestas;
+    }
 }
--- projetosistemabuffet/src/models/Funcionario.java
+++ projetosistemabuffet/src/models/Funcionario.java
@@ -1,5 +1,8 @@
 package models;
 
 public abstract class Funcionario {
-    
+    private int id;
+    private String cpf;
+    private String nome;
+    private String telefone; 
 }
